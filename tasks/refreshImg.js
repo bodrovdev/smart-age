66d3446c7d2bd1d4c867dc5de229b67ee33d3e39
Igor Bodrov <bodrovigor02@gmail.com>
2023-09-18 10:21:28 +0300

    fix: 🐛 bugfix

--- tasks/refreshImg.js
+++ tasks/refreshImg.js
@@ -2,7 +2,7 @@ const { src, dest } = require('gulp');
 
 // Обновление изображений в папке build
 module.exports = function refreshImg(browserSync) {
-	return src('src/img/image/**/*.+(png|jpg|jpeg|gif|svg|ico|webp|mp4)')
-		.pipe(dest('build/img/image'))
+	return src('src/img/**/*.+(png|jpg|jpeg|gif|svg|ico|webp|mp4)')
+		.pipe(dest('build/img'))
 		.pipe(browserSync.stream())
 };
